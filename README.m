807f30ae3797159828c905e32b6da5a5ff337b79
诗意天城 <lf_sytc@hotmail.com>
2019-01-10 10:40:01 +0800

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,10 @@
-# TechnicalManager
+# iOS TechnicalManager
+
+工作职能：
+1. 技术建设
+2. 团队建设
+
+## 工具
+
+1. 多余图片检测 https://github.com/tinymind/LSUnusedResources
+2. 多余文件检测 https://github.com/CatchZeng/CATClearProjectTool
